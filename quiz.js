a5d5a4dd5ac03db3aaf6cc0d2ad388cf444f1070
Melissa Buckingham <m19b88@gmail.com>
2023-10-23 10:54:01 +0000

    Added Quiz to the nav bar

--- quiz.js
+++ quiz.js
@@ -87,3 +87,7 @@ function checkAns() {
         nextQuestion();
     }
 }
+
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
